65e687774de900b9bc7288a68902abf36b623714
Yanone <post@yanone.de>
2020-01-16 13:54:19 +0100

    Corrected build script

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
 TTFDIR="./TTFs"
 rm -r $TTFDIR/*.ttf
 
-fontmake -g ./Glyphs/sen.glyphs -o ttf -i --output-dir $TTFDIR
+fontmake -g ./Glyphs/sen.glyphs -o ttf -i --output-dir $TTFDIR -a
 
 for f in $TTFDIR/*.ttf
 do
 	gftools fix-dsig --autofix $f
-	gftools fix-nonhinting $f $f.fix
+	gftools fix-hinting $f $f.fix
 	mv $f.fix $f
-	rm ${f//.ttf/-backup-fonttools-prep-gasp.ttf}
 done
 
 # # Clean up
